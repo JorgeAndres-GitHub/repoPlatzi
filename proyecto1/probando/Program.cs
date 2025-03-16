3ee78683bd4162eaa2365dcd3a889580cb775451
Jorge Andres <jorgeandresherreramonsalve@gmail.com>
2025-03-16 00:28:24 -0500

    nuevos cambios again

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Platzi!");
 
-string stripe_api_key="sk_test_3r421fsdf243fdds";
+string stripe_api_key="sk_test_4eC39HqLyjWDarjtT1zdp7dc";
